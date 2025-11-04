1c285e7efb5b54f82b6edfadebf8dda26f98c901
Pedro Ramirez <prlara23@bu.com>
2025-11-04 11:57:32 -0600

    add salary calculator

--- exercise6_salaryProgram/src/Employee.java
+++ exercise6_salaryProgram/src/Employee.java
@@ -1,2 +1,52 @@
 public class Employee {
+    private String name;
+    private int age;
+    private String department;
+    private double salary;
+    private int hourlyRate;
+    private int hoursWorked;
+
+    // setters and getters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void calculateSalary(int overtimeHours) {
+        salary = hourlyRate * hoursWorked + (hourlyRate * overtimeHours * 1.5);
+    }
+
+    public String toString() {
+        return "Employee: " + name + ", age: " + age + ", department: " + department + ", salary: " + salary;
+    }
 }
--- exercise6_salaryProgram/src/Main.java
+++ exercise6_salaryProgram/src/Main.java
@@ -1,5 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    var employee = new Employee();
 
+    employee.setName("John");
+    employee.setAge(30);
+    employee.setDepartment("Engineering");
+    employee.setHourlyRate(100);
+    employee.setHoursWorked(40);
+    employee.calculateSalary(20);
+    System.out.println(employee.toString());
 }
